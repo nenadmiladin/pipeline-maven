90edf48ee1226a57556d722c332f2e0b4cf3a675
nenamiladin <nenadmiladin@yahoo.com>
2020-10-20 12:58:26 +0200

    Fixed path for workspace

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,5 +3,8 @@
 echo "*************************"
 echo "      Building jar"
 echo "*************************"
-docker run --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@" # mvn -B -DskipTest clean package
+
+WORKSPACE=/home/nenad/jenkins-data/jenkins_home/workspace
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@" # mvn -B -DskipTest clean package
 
